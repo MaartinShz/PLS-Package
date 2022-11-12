79ebe3b60ca5210b83d63e271337f4f2a9f14f03
kenan reynaud <kenan.reynaud@gmail.com>
2022-11-12 15:00:33 +0100

    scale et une tentative de plot

--- plsda/R/scale.R
+++ plsda/R/scale.R
@@ -14,7 +14,23 @@
 #' @examples
 #' y = get_dummies(iris$Species)
 
+
 scale<-function (x, reduce=T){
 
+    Mat<-as.matrix(x)
+    if(typeof(Mat)!="double"){
+      stop("error in matrix")
+    }
+    means<-apply(Mat,2,mean)
+
+    if(ncol(Mat)>1){newMat<-t(apply(Mat,1,function(x){ return(x-means)}))
+    }else{newMat <- Mat-means
+    }
+    if(reduce){vars<-apply(Mat,2,var)
+      newMat <- t(apply(newMat,1,function(x){x/sqrt(vars)}))
+      return(list("New"=as.matrix(newMat), "means"=means, "vars"=vars))
+    }else{
+      return(list("New"=as.matrix(newMat),"means"=means))
+    }
 
 }
