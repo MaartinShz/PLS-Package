4fa8b80a56bdba9af1d2a94bb9fdbd2a98837e92
unknown <maartinschultz@gmail.com>
2022-11-25 17:47:53 +0100

    fit

--- R/plsda_fit.R
+++ R/plsda_fit.R
@@ -27,81 +27,107 @@ plsda_fit<-function(ObjectPLSDA, var.cible, data, ncomp=NULL, var.select = F, ce
   if(is.null(ncomp)){
     ncomp = a
   }
+  ###########################
 
   # selection de X et Y
   x= data[,-ncol(data)]
   Y= var.cible
 
   if(centre){
-  # centre-reduire les explicatives
-  x = scale(x)
+    # centre-reduire les explicatives
+    x = scale(x)
   }
 
 
   # recodage de la variable cible
   y = get_dummies(Y)
+  y = scale(y)
 
+
+  ###########################  ###########################
 
   #matrice des poids des composantes de X
-  w = data.frame(matrix(rep(0), nrow = ncol(x), ncol=ncomp))
-  rownames(w) = colnames(x)
+  Xweights = data.frame(matrix(rep(0), nrow = ncol(x), ncol=ncomp))
+  rownames(Xweights) = colnames(x)
 
   #matrice des composantes de X # loadings # var latentes
-  P = data.frame(matrix(rep(0), nrow = ncol(x), ncol=ncomp))
-  rownames(P) = colnames(x)
+  Xloadings = data.frame(matrix(rep(0), nrow = ncol(x), ncol=ncomp))
+  rownames(Xloadings) = colnames(x)
 
   #matrice des scores de X
-  t = data.frame(matrix(rep(0), nrow = nrow(x), ncol=ncomp))
+  Xscores = data.frame(matrix(rep(0), nrow = nrow(x), ncol=ncomp))
 
   #matrice des scores de Y
-  u = data.frame(matrix(rep(0), nrow = nrow(x), ncol=ncomp))
+  Yscores = data.frame(matrix(rep(0), nrow = nrow(x), ncol=ncomp))
 
   #matrice des composantes de Y # loadings # var latentes
-  q = data.frame(matrix(rep(0), nrow = ncol(y), ncol = ncomp))
-  rownames(q) = colnames(y)
+  Yloadings = data.frame(matrix(rep(0), nrow = ncol(y), ncol = ncomp))
+  rownames(Yloadings) = colnames(y)
+
+  ###########################  ###########################
+
+  w=0.5
 
   for(k in 1:ncomp){
     u <- as.matrix(y[,1])
-    w.old = 1
-    while(abs(sum(w.old^2)-sum(w^2)) > 1e-10){
 
-      w = (t(x) %*% u) / (sum(u^2)) #t(u) %*% u #dim(4 1)
+    while(abs(1-sum(w^2)) > 1e-10){
+
+      w = (t(x) %*% u) / (sum(u^2)) #t(u) %*% u #dim(4 1)   #matrice des poids des composantes de X
       w = w / sqrt(sum(w^2)) # normalization de w
 
-      z = t(x)
       #browser()
 
-      t = x %*% w / (sum(w^2)) #t(x)
-      q = (t(y) %*% t) / (sum(t^2))
-      u = (y %*% q) / (sum(q^2)) #t(y)
+      t = x %*% w / (sum(w^2)) #t(x) #matrice des scores de X
+      q = (t(y) %*% t) / (sum(t^2))  #matrice des composantes de Y # loadings
+
+      u = (y %*% q) / (sum(q^2)) #t(y) #matrice des scores de Y
     }
-    P = (t(x) %*% t) / (sum(t^2))
+
+    P = (t(x) %*% t) / (sum(t^2)) #matrice des composantes de X # loadings
     x = x - t %*% t(P)
     y = y - t %*% as.matrix(t(q))
+
+
+      #stockage des colonnes
+
+  Xweights[, k] <- w #
+  Xscores[, k] <- t #
+  Yscores[, k] <- u
+  Xloadings[, k] <- P
+  Yloadings[, k] <- q
+
+
   }
 
 
- x_rotation = w%*%solve(t(P)%*%w)
- coef = x_rotation%*%t(q)
- coef = coef*sapply(Y,f = sd)
- intercept = colMeans(Y)
+
+
+
+ #x_rotation = w%*%solve(t(P)%*%w)
+ #coef = x_rotation%*%t(q)
+ #ecart = sapply(y,FUN= sd)
+
+ #coef = coef*ecart
+ #intercept = colMeans(y)
 
 
  obj = list("x"=x,
             "y"=y,
-            "y_loadings"=q,
-            "y_scores"=u,
-            "x_loadings"=P,
-            "x_weights"=w,
-            "x_scores"=t,
-            "ncomp"=ncomp,
-            "coeff"= coef,
-            "intercept"=intercept)
+            "y_loadings"=Yloadings,
+            "y_scores"=Yscores,
+            "x_loadings"=Xloadings,
+            "x_weights"=Xweights,
+            "x_scores"=Xscores,
+            "ncomp"=ncomp)#,
+            #"coeff"= coeff,
+            #"intercept"=intercept)
 
 
   return(obj)
 
 }
 
-#data = iris
-#print(plsda_fit(data$Species, data))
+data = iris
+obj = plsda()
+print(plsda_fit(obj,data$Species, data, ncomp=2))
