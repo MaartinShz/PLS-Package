908a4be4f34d91e2156cf53351f875bfaa3cc516
Maartin <maartinschultz@gmail.com>
2022-11-27 18:08:31 +0100

    update help and installation via github

--- R/plsda_predict.R
+++ R/plsda_predict.R
@@ -4,26 +4,26 @@
 #' It make a prediction on test data
 #'
 #' @usage
-#' plsda_predict(obj,xtest)
+#' plsda_predict(obj,newdata)
 #'
 #' @param
 #' object PLDA Object
-#' Xtest dataframe of the test dataset with no target variable
+#' newdata dataframe of the test dataset with no target variable
 #' type "posterior" or "class" return format of the method
 #'
 #' @return
-#' type= posterior #probabilité d’appartenance aux classes)
-#' type= class #classe prédite
+#' type= posterior to have probability of class membership by observation
+#' type= class to have the class predicted by observation
 #'
 #'
 #' @examples
 #'#obj = plsda()
 #'obj = plsda_fit(obj,iris$Species,iris,ncomp=2)
-#'xtest= iris[,-ncol(iris)]
-#'plsda_predict(obj,xtest)
+#'newdata= iris[,-ncol(iris)]
+#'plsda_predict(obj,newdata)
 #'
 #'
-plsda_predict<-function(object, Xtest, type="posterior"){
+plsda_predict<-function(object, newdata, type="posterior"){
   if (class(object)!="PLSDA") {
     stop("Object's class is not PLSDA")
   }
@@ -31,11 +31,11 @@ plsda_predict<-function(object, Xtest, type="posterior"){
   if (type != "posterior" & type != "class"){
     stop("incorrect type")
   }
-  if (!is.data.frame(Xtest)){ #check if data is a dataframe
+  if (!is.data.frame(newdata)){ #check if data is a dataframe
     stop("data must be a dataframe")
   }
 
-  x = scale(Xtest)
+  x = scale(newdata)
 
   y_pred = x%*% as.matrix(object$coefficients)# + object$intercept
 
