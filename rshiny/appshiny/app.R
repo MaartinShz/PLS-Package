a1642466c9cc0bea81cfad1c1f7363a0bff52ca8
unknown <maartinschultz@gmail.com>
2022-12-01 21:03:54 +0100

    app import + start fit and predict shiny

--- rshiny/appshiny/app.R
+++ rshiny/appshiny/app.R
@@ -21,10 +21,8 @@ ui <- fluidPage(
                                 numericInput("sheetnumber", "Sheetnumber:", 1, min = 1, max = 255),
 
                             ),
-                            mainPanel(
-                                dataTableOutput("contents")
-                                #tableOutput("contents")
-                            )
+                            mainPanel(dataTableOutput("contents"))
+                                    #tableOutput("contents")
                         )
 
 
@@ -41,17 +39,21 @@ ui <- fluidPage(
                                 radioButtons("datasplit","Data: ",choices = c("Test"="test", "Train"="train"), selected="test",inline=TRUE),
                                 numericInput("pourcentagesplit", "Pourcentage Split:", 33, min = 1, max = 99),
 
+                                tags$hr(),
+                                uiOutput("xvar"),
+                                uiOutput("yvar"),
+
 
 
                                 tags$hr(),
-                                numericInput("Number of Composent", "ncomp:", 1, min = 1),
+                                numericInput("ncomp", "Number of Composent :", 1, min = 1),
                                 checkboxInput("vip", "Selection Variables", FALSE),
                                 checkboxInput("center", "Centre-Reduire", TRUE),
                                 tags$hr(),
 
 
                             ),
-                            mainPanel()
+                            mainPanel(dataTableOutput("fit"))
                         )
 
 
@@ -87,8 +89,7 @@ server <- function(input, output) {
                 read.csv(inFile$datapath, header = input$header, sep = input$separator)
 
             } else {
-                # statement
-
+                stop("file must be csv or xlsx")
             }
         }
 
@@ -100,13 +101,59 @@ server <- function(input, output) {
     output$contents=renderDataTable({head(data(),10)})
     #output$contents=renderTable({head(data(),10)})
 
-
     output$xvar=renderUI({
-        choiceX=colnames(data())
+        NamesX=colnames(data())
         selectInput(inputId = "varx",
                     label = "Select your X variables",
-                    choices = choiceX,
+                    choices = NamesX,
                     multiple=TRUE)})
+    output$yvar=renderUI({
+        NamesY=colnames(data())
+        selectInput(inputId = "vary",
+                    label = "Select your Y variables",
+                    choices = NamesY,
+                    multiple=FALSE)})
+
+
+
+    fit = reactive({
+        if(as.logical(input$split)){
+
+            if(input$datasplit == "test"){
+                dataSplit = split_sample(data=data(), train_perc=(1-(input$pourcentagesplit*0.01)))
+            }else if (input$dataplit == "train"){
+                dataSplit = split_sample(data=data(), train_perc=(input$pourcentagesplit*0.01))
+            }
+
+
+        }
+        #print(dataSplit$train)
+
+
+        print(input$varx)
+        print(input$vary)
+
+        #ytrain =
+        #xtrain =
+
+
+
+
+
+        obj = plsda()
+        #plsda_fit(obj,xxxxxxx, xxdatatrainxxxx, ncomp=input$ncomp, var.select = as.logical(input$vip), centre=as.logical(input$center))
+
+    })
+
+
+    output$fit=renderDataTable({fit()})
+
+
+
+
+
+
+
 
 
 
