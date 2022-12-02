28a6b9a3b4d9a28bebd858a9445c21425c80901e
Maartin <maartinschultz@gmail.com>
2022-12-03 00:47:46 +0100

    comment plsda fit

--- R/plsda_fit.R
+++ R/plsda_fit.R
@@ -22,26 +22,27 @@
 #'obj = plsda()
 #'plsda_fit(obj,iris$Species, iris, ncomp=2)
 
-plsda_fit<-function(object, formula, data, ncomp=NULL, var.select = F, centre=T){
+plsda_fit<-function(object, formula, data, ncomp=NULL, var.select = F, centre=T){ # Fit function for a pls object
 
   if (class(object)!="PLSDA") {
     stop("Object's class is not PLSDA")
   }
 
-  if (!is.data.frame(data) & !is.matrix(data) & !class(data)=="model.matrix.default"){ #check if data is a dataframe
+  if (!is.data.frame(data) & !is.matrix(data) & !class(data)=="model.matrix.default"){ #check data format
     stop("data must be a dataframe")
   }
 
 
   if(is.null(ncomp) || !is.numeric(ncomp) || ncomp>min(nrow(data), ncol(data))){
+
     # if ncomp is null or in a wrong format we take the Range of the matrix
     ncomp = min(nrow(data), ncol(data))
   }
-  ###########################
 
   # selection of X the predictive data  and  Y the target dataz
   X = model.matrix(formula,data=data)[,-1] #pour enlever intercept
   Y = model.response(model.frame(formula,data=data))
+
   # recode in dummy values the target data
   y = get_dummies(Y)
 
@@ -52,50 +53,44 @@ plsda_fit<-function(object, formula, data, ncomp=NULL, var.select = F, centre=T)
   }
 
 
+  ##############################################################################
 
-
-
-
-
-  ###########################  ###########################
-
-  #matrice des poids des composantes de X
+  #matrix composants X weights
   Xweights = data.frame(matrix(rep(0), nrow = ncol(x), ncol=ncomp))
   rownames(Xweights) = colnames(x)
 
-  #matrice des composantes de X # loadings # var latentes
+  #matrix of X loadings # latents variables
   Xloadings = data.frame(matrix(rep(0), nrow = ncol(x), ncol=ncomp))
   rownames(Xloadings) = colnames(x)
 
-  #matrice des scores de X
+  #matrix X scores
   Xscores = data.frame(matrix(rep(0), nrow = nrow(x), ncol=ncomp))
 
-  #matrice des scores de Y
+  #matrix Y scores
   Yscores = data.frame(matrix(rep(0), nrow = nrow(x), ncol=ncomp))
 
-  #matrice des composantes de Y # loadings # var latentes
+  #matrix of y loadings # latants varaibles
   Yloadings = data.frame(matrix(rep(0), nrow = ncol(y), ncol = ncomp))
   rownames(Yloadings) = colnames(y)
 
-  ###########################  ###########################
-
+  ##############################################################################
   for(k in 1:ncomp){
     u <- as.matrix(y[,1])
 
-    w = (t(x) %*% u) / (sum(u^2)) #t(u) %*% u #dim(4 1)   #matrice des poids des composantes de X
-    w = w / sqrt(sum(w^2)) # normalization de w
+    w = (t(x) %*% u) / (sum(u^2)) #t(u) %*% u #matrix composants X weights
+    w = w / sqrt(sum(w^2)) # normalization of w
 
     temp=0
     iteration = 0
 
-    while(abs(mean(w)-mean(temp)) > 1e-10){
+    while(abs(mean(w)-mean(temp)) > 1e-10){ # stop if results converge
       iteration = iteration + 1
       temp=w
-      t = x %*% w / (sum(w^2)) #t(x) #matrice des scores de X
-      q = (t(y) %*% t) / (sum(t^2))  #matrice des composantes de Y # loadings
-      u = (y %*% q) / (sum(q^2)) #t(y) #matrice des scores de Y
-      w = (t(x) %*% u) / (sum(u^2)) #t(u) %*% u #dim(4 1)   #matrice des poids des composantes de X
-      w = w / sqrt(sum(w^2)) # normalization de w
+      t = x %*% w / (sum(w^2)) #matrix X scores #
+      q = (t(y) %*% t) / (sum(t^2)) #matrix Y loadings
+      u = (y %*% q) / (sum(q^2)) #t(y) #matrix Y scores#
+      w = (t(x) %*% u) / (sum(u^2)) # = / (t(u) %*% u) # matrix composants X weights
+      w = w / sqrt(sum(w^2)) #normalization of w
 
       if (iteration>500) {
         stop("PLS regression cannot converge")
@@ -103,11 +98,11 @@ plsda_fit<-function(object, formula, data, ncomp=NULL, var.select = F, centre=T)
 
     }
 
-    P = (t(x) %*% as.matrix(t)) / (sum(t^2)) #matrice des composantes de X # loadings
+    P = (t(x) %*% as.matrix(t)) / (sum(t^2)) #matrix X loadings
     x = x - t %*% t(P)
     y = y - (t %*% t(q))
 
-    #stockage des colonnes
+    #datas stock in dataframes
 
     Xweights[, k] <- w
     Xscores[, k] <- t
@@ -118,7 +113,7 @@ plsda_fit<-function(object, formula, data, ncomp=NULL, var.select = F, centre=T)
   }
 
 
-  testXrot <- tryCatch({
+  testXrot <- tryCatch({ # if the number of composant is too high calcul can't be solved
     solve(t(Xloadings)%*%as.matrix(Xweights))
   },
   error = function(err) {
